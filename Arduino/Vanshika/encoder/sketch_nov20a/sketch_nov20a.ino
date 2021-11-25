0025b98885072381a0a5c80abc27c9e5ceee3464
vanshika2407 <vanshikaeumind@gmail.com>
2021-11-25 17:46:27 +0530

    Added encoder code

---
+++ Arduino/Vanshika/encoder/sketch_nov20a/sketch_nov20a.ino
@@ -0,0 +1,42 @@
+#define inputCLK 4
+#define inputDT 5
+int counter=0; // this is the value encoder will be changing.
+int currentstateclk; //to know wether the encoder has moved or not we need current state and previous state
+int previousstateclk;
+String encidr=" "; // will show the direction of encoder
+void setup() {
+  // setting encoder pins as inputs
+  pinMode(inputCLK,INPUT);
+ pinMode(inputDT,INPUT);
+ // setting serial monitor
+ Serial.begin(9600);
+// assigning previous value of encoder.
+previousstateclk=digitalRead(inputCLK);
+}
+
+void loop() {
+  // assigning the current state of clk
+  currentstateclk=digitalRead(inputCLK);
+  // if the previous state and current state is different thus a pulse has occured
+  if(currentstateclk!=previousstateclk)
+  {
+    // if the input clk is diff from input dt then encoder rotates anticlockwise
+    if(digitalRead(inputDT)!=currentstateclk)
+    {
+      counter--;
+      encidr="ANTICLOCKWISE";
+    }
+    else
+    {
+      counter++;
+      encidr="CLOCKWISE";
+    }
+    Serial.print("Direction: ");
+    Serial.print(encidr);
+    Serial.print("--Value: ");
+    Serial.println(counter);
+  }
+// update the previous state with  current state 
+previousstateclk=currentstateclk; 
+  
+}
