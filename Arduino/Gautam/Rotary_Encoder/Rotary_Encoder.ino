a67ecae70adf84138f429a98de19d1e2c6879496
Ghostly842 <soham842@gmail.com>
2021-12-11 14:05:04 +0530

    Update Rotary_Encoder.ino

--- Arduino/Gautam/Rotary_Encoder/Rotary_Encoder.ino
+++ Arduino/Gautam/Rotary_Encoder/Rotary_Encoder.ino
@@ -1,71 +1,58 @@
-// Rotary Encoder Inputs
-https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
 #define CLK 2
 #define DT 3
 #define SW 4
-
 int counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-String currentDir ="";
-unsigned long lastButtonPress = 0;
-
-void setup() {
-  
-  // Set encoder pins as inputs
-  pinMode(CLK,INPUT);
-  pinMode(DT,INPUT);
-  pinMode(SW, INPUT_PULLUP);
-
-  // Setup Serial Monitor
+int currentstateCLK;
+int laststateCLK;
+String currentdir =" ";
+unsigned long lastbuttonpress = 0;
+void setup()
+{
+  //set encoder pin as inputs
+  pinMode (CLK, INPUT);
+  pinMode (DT, INPUT);
+  pinMode (SW, INPUT_PULLUP);
+  //setup serial monitor
   Serial.begin(9600);
-
-  // Read the initial state of CLK
-  lastStateCLK = digitalRead(CLK);
+  //read initial state of CLK
+  laststateCLK = digitalRead(CLK);
 }
-
-void loop() {
-  
-  // Read the current state of CLK
-  currentStateCLK = digitalRead(CLK);
-
-  // If last and current state of CLK are different, then pulse occurred
-  // React to only 1 state change to avoid double count
-  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-
-    // If the DT state is different than the CLK state then
-    // the encoder is rotating CCW so decrement
-    if (digitalRead(DT) != currentStateCLK) {
-      counter --;
-      currentDir ="CCW";
-    } else {
-      // Encoder is rotating CW so increment
-      counter ++;
-      currentDir ="CW";
-    }
-
+void loop()
+{
+ currentstateCLK = digitalRead(CLK);
+ if (currentstateCLK != laststateCLK && currentstateCLK == 1)
+ {
+  if (digitalRead(DT) != currentstateCLK)
+  {
+    counter --;
+    currentdir = "CCW"; 
+  }
+  else
+  {
+    counter ++;
+    currentdir = "CW";
+  }
     Serial.print("Direction: ");
-    Serial.print(currentDir);
+    Serial.print(currentdir);
     Serial.print(" | Counter: ");
     Serial.println(counter);
-  }
-
-  // Remember last CLK state
-  lastStateCLK = currentStateCLK;
-
-  // Read the button state
-  int btnState = digitalRead(SW);
+ }
+ laststateCLK = currentstateCLK;
+ // Read the button state
+ int btnState = digitalRead(SW);
 
   //If we detect LOW signal, button is pressed
-  if (btnState == LOW) {
+  if (btnState == LOW) 
+  {
     //if 50ms have passed since last LOW pulse, it means that the
     //button has been pressed, released and pressed again
-    if (millis() - lastButtonPress > 50) {
+    if (millis() - lastbuttonpress > 50) 
+    {
       Serial.println("Button pressed!");
     }
 
     // Remember last button press event
-    lastButtonPress = millis();
+    lastbuttonpress = millis();
   }
 
   // Put in a slight delay to help debounce the reading
