73803a8718fb451c851d5c7fdcdb1c07c9ee5d78
Atharv-Chandane <atharvchandane@gmail.com>
2022-06-07 18:25:32 +0530

    Added Gyro proto code for R1

--- Robots 2022/R1/R1_Final_proto_Stage2/R1_Final_proto_Stage2.ino
+++ Robots 2022/R1/R1_Final_proto_Stage2/R1_Final_proto_Stage2.ino
@@ -1,5 +1,7 @@
 #include <RoboClaw.h>
 #include <FastLED.h>
+#include "Wire.h"
+#include <MPU6050_light.h>
 
 FASTLED_USING_NAMESPACE           //for LED strip
 #define DATA_PIN    8
@@ -11,25 +13,28 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
-int target_encoder = 0;           // Drive PID
-int curval = 0;
+MPU6050 mpu(Wire);              //Initialize Gyro
+long timer = 0;
 
-int potPin = A0;
+int gyroAngleZ = 0;
+
+int target_gyro = 0;           // Drive PID
+int curval = 0;
 
 RoboClaw roboclaw(&Serial1, 10000);
 #define address1 128
 #define address2 129
 
-#define EN1A 2                     //1A 1B is for Drive PID
-#define EN1B 10
-#define EN2A 3                     //2A 2B is for PowerWindow PID
-#define EN2B 11
+//#define EN1A 2                     //1A 1B is for Drive PID
+//#define EN1B 10
+//#define EN2A 3                     //2A 2B is for PowerWindow PID
+//#define EN2B 11
 volatile int count1 = 0;
 volatile int count2 = 0;
 
-#define KP 0.7
-#define KI 0.0
-#define KD 1
+#define KP 15
+#define KI 5
+#define KD 3
 int prev_error = 0;
 
 int PID(int currentValue, int targetValue);
@@ -66,7 +71,7 @@ uint8_t counter = 0;
 
 #define BAUD 9600
 #define BAUDRATE  ((F_CPU/(BAUD*16UL)-1))
-#define pwm_range 30000
+#define pwm_range 200
 #define PS_L1 0
 #define PS_R1 1
 #define PS_L2 2
@@ -133,28 +138,28 @@ ISR(USART3_RX_vect)
 }
 
 
-void encoder1()
-{
-  if (digitalRead(EN1B) == HIGH)
-  {
-    count1++;
-  }
-  else if (digitalRead(EN1B) == LOW)
-  {
-    count1--;
-  }
-}
-void encoder2()
-{
-  if (digitalRead(EN2B) == HIGH)
-  {
-    count2++;
-  }
-  else if (digitalRead(EN2B) == LOW)
-  {
-    count2--;
-  }
-}
+//void encoder1()
+//{
+//  if (digitalRead(EN1B) == HIGH)
+//  {
+//    count1++;
+//  }
+//  else if (digitalRead(EN1B) == LOW)
+//  {
+//    count1--;
+//  }
+//}
+//void encoder2()
+//{
+//  if (digitalRead(EN2B) == HIGH)
+//  {
+//    count2++;
+//  }
+//  else if (digitalRead(EN2B) == LOW)
+//  {
+//    count2--;
+//  }
+//}
 
 //READ DATA FROM CONTROLLER
 bool ps_read() {
@@ -204,8 +209,7 @@ bool ps_read() {
   }
   if (butt[PS_LEFT] == 1)
   {
-    duty = 0;                                       //PowerWindow Duty 0
-    butt[PS_LEFT] = 0;
+   butt[PS_LEFT] = 0;
   }
   if (butt[PS_RIGHT] == 1)
   {
@@ -222,6 +226,8 @@ bool ps_read() {
   {
     setLED(0, 255, 0);                              //Button for Drive Encoder value to be set as 0
     count1 = 0;                                     //Also Hopper button count to be set as 0
+    count2 = 0;
+    target_gyro = 0;
     button_count = 0;
     counter = 0;
     butt[PS_CIRCLE] = 0;
@@ -276,13 +282,13 @@ bool ps_read() {
 
   if (butt[PS_L1] == 1)
   {
-    target_encoder++;                                  //Bot rotation by increasing target value
+    target_gyro += 10;                                  //Bot rotation by increasing target value
     Serial.println("L1");
     butt[PS_L1] = 0;
   }
   if (butt[PS_R1] == 1)
   {
-    target_encoder--;                                  //Bot rotation by decreasing target value
+    target_gyro -= 10;                                  //Bot rotation by decreasing target value
     Serial.println("R1");
     butt[PS_R1] = 0;
   }
@@ -315,11 +321,11 @@ void drive(double A_x, double A_y, double ang_s)         //Drive Equation
   */
   F1 = (0.5773 * A_x) + (0.333 * A_y) + (0.333 * ang_s);
   F2 = -(0.5773 * A_x) + (0.333 * A_y) + (0.333 * ang_s);
-  F3 = (0 * A_x) - (0.666 * A_y) + (0.333 * ang_s);
+  F3 = (0 * A_x) + (0.666 * A_y) - (0.333 * ang_s);
 
-  F1 = map(F1, -127, 127, -32000, 32000);
-  F2 = map(F2, -127, 127, -32000, 32000);
-  F3 = map(F3, -127, 127, -32000, 32000);
+  F1 = map(F1, -200, 200, -32000, 32000);
+  F2 = map(F2, -200, 200, -32000, 32000);
+  F3 = map(F3, -200, 200, -32000, 32000);
 
   roboclaw.DutyM2(address1, -F1);
   roboclaw.DutyM2(address2, -F2);
@@ -340,7 +346,7 @@ void powerwindowDuty()                                    //PowerWindow Throwing
   roboclaw.DutyM1(address1, duty);
 }
 
-int PID(int currentValue, int targetValue)                //PID code
+int PID(int currentValue, int targetValue)                //PID code block
 {
   int error = targetValue - currentValue;
 
@@ -363,26 +369,26 @@ void firstActuation() {                                  //Hopper Actuation
   Serial.println("First");                               //First Middle Piston is actuated
   digitalWrite(pistonCenterOut, HIGH);                   //Then left Motor for pushing the ball is turned ON
   digitalWrite(pistonCenterIn, LOW);
-  curval = PID((count1), target_encoder);                //Then left piston is actuated
+  curval = -PID((gyroAngleZ), target_gyro);                //Then left piston is actuated
   drive(0, 0, curval);                                   //Left motor is turned OFF
   ps_read();
   delay_ms(500);
   ps_read();
   digitalWrite(pistonCenterOut, LOW);
   digitalWrite(pistonCenterIn, HIGH);
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   ps_read();
   delay_ms(2000);
   ps_read();
   digitalWrite(pistonLeft, HIGH);
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   motorLeft(1, 255);
   ps_read();
   delay_ms(3000);
   ps_read();
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   digitalWrite(pistonLeft, LOW);
   motorLeft(0, 0);
@@ -392,26 +398,26 @@ void secondActuation() {                                  //Hopper Actuation
   Serial.println("Second");                               //Middle Piston is actuated
   digitalWrite(pistonCenterIn, LOW);
   digitalWrite(pistonCenterOut, HIGH);                     //Then right motor for pushing the ball is turned ON
-  curval = PID((count1), target_encoder);                 //Then right piston is actuated
+  curval = -PID((gyroAngleZ), target_gyro);                 //Then right piston is actuated
   drive(0, 0, curval);                                    //Right motor is turned OFF
   ps_read();
   delay_ms(500);
   ps_read();
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   digitalWrite(pistonCenterOut, LOW);
   digitalWrite(pistonCenterIn, HIGH);
   ps_read();
   delay_ms(2000);
   ps_read();
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   digitalWrite(pistonRight, HIGH);
   motorRight(1, 255);
   ps_read();
   delay_ms(3000);
   ps_read();
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   digitalWrite(pistonRight, LOW);
   motorRight(0, 0);
@@ -424,7 +430,7 @@ void thirdActuation() {                                    //Middle Piston is Ac
   ps_read();
   delay_ms(500);
   ps_read();
-  curval = PID((count1), target_encoder);
+  curval = -PID((gyroAngleZ), target_gyro);
   drive(0, 0, curval);
   digitalWrite(pistonCenterOut, LOW);
   digitalWrite(pistonCenterIn, HIGH);
@@ -482,15 +488,22 @@ void loadingLEDrev(int r, int g, int b, int ms) {
 
 void setup()
 {
+  ps_setup();
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   setLED(255, 0, 0);
 
-  ps_setup();
+  Wire.begin();
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  mpu.calcOffsets(true,true); // gyro and accelero
+  Serial.println("Done!\n");
+
   roboclaw.begin(115200);
   Serial.begin(115200);
-
-  pinMode(potPin, INPUT);
 
   pinMode(pistonCenterOut, OUTPUT);
   pinMode(pistonCenterIn, OUTPUT);
@@ -504,28 +517,33 @@ void setup()
   pinMode(uForward, OUTPUT);
   pinMode(uBackward, OUTPUT);
 
-  pinMode(EN1A, INPUT_PULLUP);
-  pinMode(EN1B, INPUT_PULLUP);
-  pinMode(EN2A, INPUT_PULLUP);
-  pinMode(EN2B, INPUT_PULLUP);
+//  pinMode(EN1A, INPUT_PULLUP);
+//  pinMode(EN1B, INPUT_PULLUP);
+//  pinMode(EN2A, INPUT_PULLUP);
+//  pinMode(EN2B, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(EN1A), encoder1, RISING);
-  attachInterrupt(digitalPinToInterrupt(EN2A), encoder2, RISING);
-  delay(1000);
+//  attachInterrupt(digitalPinToInterrupt(EN1A), encoder1, RISING);
+//  attachInterrupt(digitalPinToInterrupt(EN2A), encoder2, RISING);
+  delay_ms(1000);
   setLED(0, 255, 0);
 
 }
 
 void loop()
 {
-  int potVal = analogRead(potPin);
+
+//  if (RX_count == 1){
+ 
+  mpu.update();
   
   duty = 0;
   ps_read();
 
   powerwindowDuty();
-
-  curval = PID((count1), target_encoder);
+  
+  gyroAngleZ = mpu.getAngleZ();
+  curval = -PID((gyroAngleZ), target_gyro);
+//  drive(xj1, -yj1, yj2);
   drive(0, 0, curval);
 
   if (button_count == 1 && counter == 0) {
@@ -545,31 +563,10 @@ void loop()
     thirdActuation();
   }
 
-  //  Serial.print("PID pot value\t");
-  //  Serial.print(motor_val);
-  Serial.print("PW duty \t");
-  Serial.print(duty);
-  Serial.print("\t enc count \t");
-  Serial.print((count1));
-  Serial.print("\t PW enc value \t");
-  Serial.print(count2);
-  
-
+  Serial.print(" Gyro_Z \t");
+  Serial.print(mpu.getAngleZ());
+  Serial.print("\t PID \t");
+  Serial.print(PID((gyroAngleZ), target_gyro));
+//  }  
 
 }
-
-//void loop()
-//{
-//  loadingLED(255, 0, 0, 500);
-//  delay(100);
-//  loadingLEDrev(255, 255, 0,500);
-//  delay(100);
-//  ps_read();
-//  Serial.print(xj1);
-//  Serial.print("\t");
-//  Serial.print(xj2);
-//  Serial.print("\t");
-//  Serial.print(yj1);
-//  Serial.print("\t");
-//  Serial.println(yj2);
-//}
